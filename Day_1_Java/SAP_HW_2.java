c4552f2058be217ea16be85e255dc35be7b234fe
Martin Jordanov <xxm0703@abv.bg>
2018-09-05 16:05:11 +0300

    Battery charge

--- Day_1_Java/SAP_HW_2.java
+++ Day_1_Java/SAP_HW_2.java
@@ -1,43 +1,85 @@
 package Day_1_Java;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class SAP_HW_2 {
     public static void main(String[] args) {
-        int[] num = new int[4];
-        boolean duplicate = false;
-        do {
-            for (int i = 0; i < 4; i++) {
-                num[i] = (int) (Math.random() * 10);
-            }
-            for (int i = 0; i < 4; i++) {
-                int[] test = num.clone();
-                test[i] = -1;
-                duplicate = SAP_HW_2.int_in_list(test, num[i]);
-            }
-        } while (num[0] == 0 || duplicate);
-        Scanner sc = new Scanner(System.in);
-        int digit, cow, bull;
-        do {
-            cow = 0;
-            bull = 0;
-            int guess = sc.nextInt();
-            for (int i = 3; i >= 0; i--) {
-                digit = guess % 10;
-                guess /= 10;
-                if (SAP_HW_2.int_in_list(num, digit)) {
-                    if (digit == num[i]) bull++;
-                    else cow++;
-                }
-            }
-            System.out.println(String.format("Cows: %d\tBulls: %d", cow, bull));
-        } while (bull != 4);
+        Game g = new Game();
+        g.play();
     }
 
-    private static boolean int_in_list(int[] l, int s) {
-        for (int element : l) {
-            if (element == s) return true;
+    protected static class Game {
+        private ArrayList<Byte> num = new ArrayList<>();
+        private ArrayList<Byte> guess = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+
+        Game() {
+            do {
+                this.num.clear();
+                for (int i = 0; i < 4; i++) {
+                    this.num.add((byte) (Math.random() * 10));
+                }
+            } while (this.notValid(this.num));
         }
-        return false;
+
+        Game(short num) {
+            this.num = transform(num);
+            if (this.notValid(this.num)) throw new NumberFormatException();
+
+        }
+
+        private boolean notValid(ArrayList<Byte> l) {
+            if (l.get(0) == 0) return true;
+            for (int i = 0; i < 3; i++) {
+                if (l.subList(i + 1, l.size()).contains(l.get(i))) return true;
+            }
+            return false;
+        }
+
+        private ArrayList<Byte> transform(short num) {
+            ArrayList<Byte> l = new ArrayList<>();
+            for (int i = 0; i < this.num.size(); i++) {
+                l.add((byte) 0);
+            }
+            for (int i = this.num.size() - 1; i >= 0; i--) {
+                l.set(i, (byte) (num % 10));
+                num /= 10;
+            }
+            return l;
+        }
+
+        void play() {
+            do {
+                this.guessing(sc.nextShort());
+            } while (this.bulls() < 4);
+
+            System.out.println("You WIN!");
+        }
+
+        private void guessing(short g) {
+            this.guess = this.transform(g);
+            if (this.notValid(this.guess)) throw new NumberFormatException();
+            System.out.printf("Bulls: %d\nCows: %d\n", this.bulls(), this.cows());
+
+        }
+
+        private byte bulls() {
+            byte count = 0;
+            for (int i = 0; i < this.num.size(); i++) {
+                if (this.num.get(i).equals(this.guess.get(i))) count++;
+            }
+            return count;
+        }
+
+        private byte cows() {
+            byte count = 0;
+            for (int i = 0; i < this.num.size(); i++) {
+                if (!this.num.get(i).equals(this.guess.get(i)) && this.num.contains(this.guess.get(i))) count++;
+            }
+            return count;
+        }
     }
 }
